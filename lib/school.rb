07410eeddaf23d834ddc1b4e4be64074257e50bb
Stacy Johns <stacykjohns@icloud.com>
2019-08-01 13:17:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,9 @@
 class School
+  attr_reader :roster
   def initialize(school_name)
     @school = school_name
     @roster = {}
   end
-
-  attr_reader :roster
 
   def add_student(name, grade)
     roster[grade] ||= []
